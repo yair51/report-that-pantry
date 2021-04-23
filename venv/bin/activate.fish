cff156c56cde164e3062516a282ddde05dfe182c
Yair Gritzman <yairgritman@gmail.com>
2021-04-22 21:21:01 -0400

    Created new venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yairgritzman/web-projects/techwtim_github_flask/venv"
+set -gx VIRTUAL_ENV "/Users/yairgritzman/web-projects/fau_website_2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
