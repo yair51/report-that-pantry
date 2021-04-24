7d80e7edbd8b460a5afc322c7aa4ee5aab61b480
Yair Gritzman <yairgritman@gmail.com>
2021-04-23 21:56:01 -0400

    changing machines

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yairgritzman/web-projects/fau_website_2/venv"
+set -gx VIRTUAL_ENV "/Users/yairgritzman/web-projects/fau_website_2-1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
