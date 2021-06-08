ac9690695615d89845f53feba764656c8a782ced
Yair Gritzman <yairgritman@gmail.com>
2021-06-07 20:05:46 -0400

    removed note table

--- website/models.py
+++ website/models.py
@@ -4,11 +4,11 @@ from sqlalchemy.sql import func
 from datetime import datetime
 
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+# class Note(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     data = db.Column(db.String(10000))
+#     date = db.Column(db.DateTime(timezone=True), default=func.now())
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
