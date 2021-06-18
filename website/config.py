7566801947cf88d8dc13a3c4de1680c86ad7c039
Yair Gritzman <yairgritman@gmail.com>
2021-06-18 15:16:10 -0400

    added secret key to .env file

--- website/config.py
+++ website/config.py
@@ -5,9 +5,10 @@ load_dotenv()
 class Config(object):
     DEBUG = False
     DEVELOPMENT = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dfal;dfkad adf'
+    SECRET_KEY = os.getenv("SECRET_KEY", "dfal;dfkad adf")
+    #SECRET_KEY = os.environ.get('SECRET_KEY') or 'dfal;dfkad adf'
     SQLALCHEMY_DATABASE_URI = 'postgresql://qtpqgzckodngbz:3c43be88e6456a266d1b60c4795ad491661d3c5f1a0920820598d8580efa960d@ec2-107-22-83-3.compute-1.amazonaws.com:5432/dbs7vbee5trfb5'
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    #SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class StagingConfig(Config):
